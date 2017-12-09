85041d90fb5e340763d353a482ad4bcda26d8efb
Juan Sánchez Almendro <juansanalme@gmail.com>
2017-12-09 17:08:27 +0100

    Modelos de puja, oferta y pedido
    Me faltan los assembler de cada uno
    Y luego ver que atributos hay que cambiar y demas

---
+++ DSM1Gen/WebDSM/Models/Pedido.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebDSM.Models
+{
+    public class Pedido
+    {
+
+        [ScaffoldColumn(false)]
+        public int id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string descripcion { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime? fecha { get; set; }
+
+        /* ILIST
+        [ScaffoldColumn(false)]
+        public LineaPedido lineaPedido { get; set; }
+        */
+
+        [ScaffoldColumn(false)]
+        public Registrado registrado { get; set; }
+
+        /*
+        [ScaffoldColumn(false)]
+        public Carrito carrito { get; set; }
+        */
+
+    }
+}
