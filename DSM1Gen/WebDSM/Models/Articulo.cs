74693959ea706d39877f7e1f738e689bb603a912
DatrixZ <datrixz997@gmail.com>
2017-12-19 09:12:07 +0100

    ViewModel
    View Articulo + Opinion

--- DSM1Gen/WebDSM/Models/Articulo.cs
+++ DSM1Gen/WebDSM/Models/Articulo.cs
@@ -53,14 +53,6 @@ namespace WebDSM.Models
 
         [ScaffoldColumn(false)]
         public String NombreCategoria { get; set; }
-        
-
-    
-
-
-
-
-
 
     }
 }
---
+++ DSM1Gen/WebDSM/Models/ArticuloYOpinion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebDSM.Models
+{
+    public class ArticuloYOpinion
+    {
+        public ArticuloYOpinion()
+        {
+            articulo = new Articulo();
+            valoracion = new Valoracion();
+        }
+        public Articulo articulo {get; set;}
+        public Valoracion valoracion {get; set;}
+    }
+}
