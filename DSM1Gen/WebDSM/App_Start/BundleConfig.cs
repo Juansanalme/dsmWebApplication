e2538506b25926c0ee4d1bffecfb795798ad8abb
DatrixZ <datrixz997@gmail.com>
2017-12-25 05:00:16 +0100

    Vamo a cssear

--- DSM1Gen/WebDSM/App_Start/BundleConfig.cs
+++ DSM1Gen/WebDSM/App_Start/BundleConfig.cs
@@ -20,6 +20,7 @@ namespace WebDSM
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/popper.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
